6f4ba901cfb7d4d3a22ca9cbbaac1a76d0126a0d
kevinlibuit <kevin.libuit@dgs.virginia.gov>
2019-06-19 19:54:40 -0400

    update local variable

--- lib/tredegar.py
+++ lib/tredegar.py
@@ -49,7 +49,7 @@ def main():
     shovill_obj = run_shovill.Shovill(path=path, output_dir=output_dir)
     shovill_obj.shovill()
 
-    quast_obj = run_quast.Quast(path=self.path, output_dir=self.output_dir)
+    quast_obj = run_quast.Quast(path=path, output_dir=output_dir)
     quast_obj.quast()
 
     cgpipeline_obj = run_cg_pipeline.CGPipeline(path=path,output_dir=output_dir)
